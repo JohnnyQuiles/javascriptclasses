4abd668597bc141b232e7b818a05f20a98523767
Johnny Quiles <79813390+JohnnyQuiles@users.noreply.github.com>
2022-04-10 14:12:14 -0400

    debugging myDBObject. Now working on USERS

--- classes.js
+++ classes.js
@@ -2,63 +2,70 @@ const { uuid } = require("uuid");
 
 class dbObject {
     createDate;
-    lastModifiedDate;
-    lastModifiedEvents;
+    modifiedDate;
+    modifiedEvents;
     uuid;
 
     constructor() {
         this.createDate = new Date().getTime();
-        this.lastModifiedDate = new Date().getTime();
-        this.lastModifiedEvents = [this.createDate];
-        this.uuid = uuid;
+        this.modifiedDate = new Date().getTime();
+        this.modifiedEvents = [this.createDate];
+        this.uuid = this.uuid;
     }
     //GET =========================================================
     createdDate() {
+        this.createDate;
         return this.createDate;
     }
     modifiedDate() {
-        return this.lastModifiedDate;
+        this.modifiedDate;
+        return this.modifiedDate;
     }
     modifiedEvents() {
-        const getAll = this.lastModifiedEvents
-        return getAll;
+        this.modifiedEvents
+        return this.modifiedEvents;
     }
     uuid() {
+        this.uuid;
         return this.uuid;
     }
+
     //SET =========================================================  
-    setGetDate() {
-        return new Date(this.date).toISOString();
-    }
-    setLastModifiedDate() {
+    setDate() {
         const newDate = new Date().getTime();
-        this.lastModifiedDate = newDate;
-        this.lastModifiedEvents.push(newDate);
+        this.createDate = newDate;
+        return new Date(this.createDate).toISOString();
     }
     setModifiedDate() {
-        return new Date(this.lastModifiedDate).toISOString();
+        const newDate = new Date().getTime();
+        this.modifiedDate = newDate;
+        this.modifiedEvents.push(newDate);
+        return new Date(this.modifiedDate).toISOString();
     }
-    setLastModifiedEvents() {
-        const events = this.lastModifiedEvents.map(() => {
+    setModifiedEvents() {
+        const events = this.modifiedEvents.map(() => {
             return new Date().toISOString();
         });
         return events;
     }
     setUUID() {
-        const id = uuid();
+        const id = this.uuid;
         this.uuid = id;
-        this.setLastModifiedDate();
     }
 };
 const myDbObject = new dbObject();
 console.log("CREATED DATE:", myDbObject.createdDate());
-console.log("SET LAST DATE:", myDbObject.setLastModifiedDate());
-console.log("SET MOD DATE:", myDbObject.setModifiedDate());
-console.log("MOD DATE:", myDbObject.modifiedDate());
-console.log("EVENT:", myDbObject.modifiedEvents());
-console.log("LAST MOD EVENT:", myDbObject.setLastModifiedEvents());
-console.log("UUID:", myDbObject.uuid);
+console.log("SET CREATED DATE:", myDbObject.setDate());
+console.log("EVENT:",myDbObject.setModifiedEvents());
 console.log("");
+
+console.log("SET LAST DATE:", myDbObject.modifiedDate);
+console.log("MOD DATE:", myDbObject.setModifiedDate());
+console.log("EVENT:", myDbObject.setModifiedEvents());
+console.log("");
+
+console.log("UUID:", myDbObject.uuid);
+console.log("SET UUID:", myDbObject.setUUID());;
 console.log("----------------------------------------------------");
 
 class User extends dbObject {
@@ -67,59 +74,67 @@ class User extends dbObject {
     favoriteMovies;
 
     constructor(name, age, favoriteMovies) {
-        super();
+        super(name, age, favoriteMovies);
         this.name = name;
         this.age = age;
         this.favoriteMovies = [favoriteMovies]
     }
-
-    get name() {
+    //GET =================================================
+    get createName() {
+        this.name;
         return this.name;
     }
-    get age() {
+    get createAge() {
+        this.age;
         return this.age;
     }
-    get favoriteMovies() {
+    get createFavoriteMovies() {
+        this.favoriteMovies;
         return this.favoriteMovies;
     }
-    set name(name) {
+    //SET ===============================================
+    set createName(name) {
         this.name = name;
-        this.setLastModifiedDate();
+        return this.name; 
     }
-    set age(age) {
-        this.age = age;
-        this.setLastModifiedDate();
-    }
-    set favoriteMovies(favoriteMovies) {
-        this.favoriteMovies = favoriteMovies;
-        this.setLastModifiedDate();
-    }
+    // set Age(age) {
+    //     this.age = age;
+    //     this.setmodifiedDate();
+    // }
+    // set favoriteMovies(favoriteMovies) {
+    //     this.favoriteMovies = favoriteMovies;
+    //     this.setmodifiedDate();
+    // }
 }
 const myUser = new User("Johnny", 26, "Jay");
 console.log("USER", myUser);
 console.log("");
-console.log("LAST MOD USER:", this.lastModifiedEvents());
+
+console.log("LAST MOD USER:", this.modifiedEvents());
 console.log("GET UUID", myUser.uuid);
 console.log("");
+
 console.log("Get NAME:", myUser.name);
 console.log("");
+
 console.log("Get AGE:", myUser.age);
 console.log("");
+
 console.log("GET FAV MOVIES:", myUser.favoriteMovies);
 console.log("");
 console.log("=====================================================");
 
-myUser.name="Serena"
-console.log("GET UPDATED NAME:", myUser.name);
-console.log("");
+// myUser.name="Serena"
+// console.log("GET UPDATED NAME:", myUser.name);
+// console.log("");
 
-myUser.age = 4;
-console.log("GET UPDATED AGE:", myUser.age);
-console.log("");
+// myUser.age = 4;
+// console.log("GET UPDATED AGE:", myUser.age);
+// console.log("");
 
-myUser.favoriteMovies = "Turning Red";
-console.log("GET UPDATED FAV MOVIE:", myUser.favoriteMovies);
-console.log("");
+// myUser.favoriteMovies = "Turning Red";
+// console.log("GET UPDATED FAV MOVIE:", myUser.favoriteMovies);
+// console.log("");
 
-console.log(myUser.lastModifiedEvents)
-console.log("MY USER:", myUser);
+// console.log(myUser.modifiedEvents)
+// console.log("MY USER:", myUser);
